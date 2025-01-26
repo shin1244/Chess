00d1200f6dd7650515da88443388fdce23b7a5e5
shin1244 <tlswnsrb1357@gmail.com>
2025-01-27 04:42:37 +0900

    브랜치 병합하다 2시간 작업 날릴뻔했다 참 재미있었다.

--- states/state0.go
+++ states/state0.go
@@ -3,47 +3,29 @@ package states
 import (
 	"chess/game"
 	"chess/ws"
+	"log"
 
 	"github.com/gorilla/websocket"
 )
 
 // 유저가 게임에 참여하기를 기다립니다. 2명이 참여하면 state1로 이동합니다.
-func State0(rooms map[int]*game.Context, conn *websocket.Conn, message game.Message) {
+func State0(g *game.Context, conn *websocket.Conn, message game.Message) {
 	if message.Type != "join" {
 		return
 	}
-	roomId := joinRoom(rooms, conn)
-	room := rooms[roomId]
+	log.Println("게임 참가", conn.RemoteAddr())
+	g.PlayerColor[conn] = len(g.PlayerColor)
 
 	conn.WriteJSON(&game.Message{
 		Type:        "color",
-		PlayerColor: room.PlayerColor[conn],
+		PlayerColor: g.PlayerColor[conn],
 	})
-	if len(room.PlayerColor) == 2 {
+	if len(g.PlayerColor) == 2 {
 		conn.WriteJSON(&game.Message{
 			Type:  "board",
-			Board: room.Board,
+			Board: g.Board,
 		})
-		room.GameState = 1
-		ws.BroadcastBoard(room, true)
+		g.GameState = 1
+		ws.BroadcastBoard(g, true)
 	}
-}
-
-func joinRoom(rooms map[int]*game.Context, conn *websocket.Conn) int {
-	joinGame := false
-	// 빈 방이 있는지 확인
-	for _, room := range rooms {
-		if len(room.PlayerColor) < 2 {
-			room.PlayerColor[conn] = len(room.PlayerColor)
-			joinGame = true
-			return len(rooms)
-		}
-	}
-	// 빈 방이 없으면 새 방 생성
-	if !joinGame {
-		rooms[len(rooms)] = game.InitGame()
-		rooms[len(rooms)].PlayerColor[conn] = len(rooms[len(rooms)].PlayerColor)
-		return len(rooms)
-	}
-	return -1
 }
